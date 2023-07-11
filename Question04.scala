c0a520c0763aad9ce0717901abc061c834334c2b
Vishwa Sandaruwan <116142492+itsvishwa@users.noreply.github.com>
2023-07-11 06:38:02 +0530

    Update Question04.scala

--- Question04.scala
+++ Question04.scala
@@ -1,11 +1,12 @@
-def even(n: Int): Boolean = n match {
+def isEven(n: Int): Boolean = n match {
     case 0 => true;
-    case 1 => false;
-    case _ => even(n-2);
+    case _ => isOdd(n-1);
 }
+
+def isOdd(n: Int): Boolean = ! (isEven(n));
 
 object Question04 extends App{
     print("Enter a number: ");
     val data = scala.io.StdIn.readInt();
-    if (even(data)) println("Even number") else println("Odd number");
+    if (isEven(data)) println("Even number") else println("Odd number");
 }
